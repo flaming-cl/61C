7588ae6a630227230d27cad38d825113eb8c4986
flaming-cl <51183663+flaming-cl@users.noreply.github.com>
2021-04-06 23:05:03 +0800

    Update relu.s

--- project/fa20-solution-fc/proj2/relu.s
+++ project/fa20-solution-fc/proj2/relu.s
@@ -14,18 +14,18 @@
 # ==============================================================================
 relu:
     ble a1, x0, invalid_len
-	li t0, 0
+    li t0, 0
 loop_start:
     lw t1, 0(a0) 			
     bge t1, x0, loop_continue
-	sw x0, 0(a0)
+    sw x0, 0(a0)
 loop_continue:
-	addi t0, t0, 1
+    addi t0, t0, 1
     beq t0, a1, loop_end
     addi a0, a0, 4
-	j loop_start
+    j loop_start
 loop_end:  
-	ret
+    ret
 invalid_len:
-	li a1, 78
+    li a1, 78
     j exit2
