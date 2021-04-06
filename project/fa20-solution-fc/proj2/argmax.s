fd6ac25cb8ca42fa760fb4ad349e800349593a22
flaming-cl <51183663+flaming-cl@users.noreply.github.com>
2021-04-06 23:06:31 +0800

    Update argmax.s

--- project/fa20-solution-fc/proj2/argmax.s
+++ project/fa20-solution-fc/proj2/argmax.s
@@ -15,12 +15,12 @@
 #   this function terminates the program with error code 77.
 # =================================================================
 argmax:
-	ble a1, x0, invalid_len # length check, if size <= 0, exit
-	li t0, 0                  # t0: counter i
+    ble a1, x0, invalid_len # length check, if size <= 0, exit
+    li t0, 0                  # t0: counter i
     li t1, 0                  # t1: max_i
     li t2, 0                  # t2: max_value
 loop_start:
-	lw t3, 0(a0)              # t3: a[i]
+    lw t3, 0(a0)              # t3: a[i]
     ble t3, t2, loop_continue # a[i] <= max_value, continue
     add t1, t0, x0            # max_i = i
     add t2, t3, x0            # max_value = a[i]
@@ -28,10 +28,10 @@ loop_continue:
     addi t0, t0, 1            # i++
     beq t0, a1, loop_end      # i == size, end
     addi a0, a0, 4
-	j loop_start
+    j loop_start
 loop_end:
-	add a0, t1, x0
+    add a0, t1, x0
     ret
 invalid_len:
-	li a1 77
+    li a1 77
     j exit2
